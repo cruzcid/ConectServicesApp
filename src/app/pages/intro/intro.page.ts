e7c373758929cf1d9901fa2b0ed150cb2e4dacd7
cruzcid <abstractime@outlook.com>
2019-04-02 11:25:11 -0600

    Intro sets users and update servicios

--- src/app/pages/intro/intro.page.ts
+++ src/app/pages/intro/intro.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonSlides } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { USERS, SERVICIOS } from '../../mock/mockdata';
+import { ServiciosService } from 'src/app/services/servicios.service';
 
 @Component({
     selector: 'app-intro',
@@ -14,7 +17,11 @@ export class IntroPage implements OnInit {
 
     @ViewChild('slides') slides: IonSlides;
 
-    constructor(private storage: Storage, private router: Router) { }
+    constructor(
+        private storage: Storage, 
+        private router: Router, 
+        private userService:UserService,
+        private serviciosService:ServiciosService) { }
 
     ngOnInit() {
     }
@@ -24,7 +31,8 @@ export class IntroPage implements OnInit {
     }
     public async saveAndStart() {
         await this.storage.set('seen-intro', true);
-        
+        await this.userService.setUsers(USERS);
+        await this.serviciosService.updateServicios(SERVICIOS);
         this.router.navigateByUrl('/');
     }
 }
